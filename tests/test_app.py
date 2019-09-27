4481d4943e9f7947fefe9414e84980acd064978a
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-09-27 14:23:07 +0200

    add login & signup tests

---
+++ tests/test_app.py
@@ -0,0 +1,66 @@
+import requests
+import urllib.parse
+import random
+import string
+from db import Users
+
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+url = "http://localhost:5000"
+email = randomString(50)
+
+
+def test_root_endpoint():
+    r = requests.get('{}/'.format(url))
+    print(r)
+    assert r.status_code == 200
+
+
+def test_signup_success():
+    query = "{signup(email: \"" + email + "\", password: \"123\") {email, token}}"
+    r = requests.get("{}/graphql?query={}".format(url,
+                                                  urllib.parse.quote(query)))
+
+    assert r.status_code == 200
+
+
+def test_signup_exists():
+    query = "{signup(email: \"john.doe@sample.net\", password: \"123\") {email, token}}"
+    r = requests.get("{}/graphql?query={}".format(url,
+                                                  urllib.parse.quote(query)))
+
+    assert len(r.json()['errors']) == 1
+    assert r.json()['errors'][0]['message'] == "User already exists"
+
+
+def test_login_success():
+    query = "{login(email:\"" + email + "\", password: \"123\") {email, token}}"
+    r = requests.get("{}/graphql?query={}".format(url,
+                                                  urllib.parse.quote(query)))
+
+    Users.delete_one({"email": email})
+    assert r.status_code == 200
+
+
+def test_login_wrong_password():
+    query = "{login(email: \"john.doe@sample.net\", password: \"1234\") {email, token}}"
+    r = requests.get("{}/graphql?query={}".format(url,
+                                                  urllib.parse.quote(query)))
+
+    assert len(r.json()['errors']) == 1
+    assert r.json()['errors'][0]['message'] == "Password incorrect"
+
+
+def test_login_no_user():
+    query = "{login(email:\"" + email + "\", password: \"123\") {email, token}}"
+    r = requests.get("{}/graphql?query={}".format(url,
+                                                  urllib.parse.quote(query)))
+
+    assert len(r.json()['errors']) == 1
+    assert r.json()['errors'][0]['message'] == "User not found"
+    
