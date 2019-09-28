14922ed39d3f4324d0d14b9b0aac3954825af2f3
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-09-28 22:10:05 +0200

    avoid api sleeping on heroku

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from flask_graphql import GraphQLView
 from graphene import Schema
 from resolvers import Query
 from flask_cors import CORS
+import schedule
+import time
+
+
+def job():
+    print("I'm working...")
+
+
+schedule.every(5).minutes.do(job)
 
 load_dotenv()
 
@@ -15,9 +24,15 @@ view_func = GraphQLView.as_view("/graphql", schema=Schema(query=Query))
 
 app.add_url_rule("/graphql", view_func=view_func)
 
+
 @app.route('/')
 def index():
     return "GraphQL server is listening on /graphql"
-    
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
 if __name__ == "__main__":
-    app.run() 
+    app.run()
